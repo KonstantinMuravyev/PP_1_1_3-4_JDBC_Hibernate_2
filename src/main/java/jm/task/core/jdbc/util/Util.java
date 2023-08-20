d3446fef198d4372e6000b5d48ac2609ca953c6a
KonstantinMuravyev <k.muravyev2016@yandex.ru>
2023-08-21 02:59:23 +1000

    hibernate implementation

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,21 +1,39 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.*;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost:3306/dbtest";
-    private static final String USERNAME = "root";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String HOST = "jdbc:mysql://localhost:3306/dbtest";
+    private static final String LOGIN = "root";
     private static final String PASSWORD = "poi48fea861";
+    private static SessionFactory sessionFactory = null;
 
-    public static Connection getConnection() {
-        Connection connection = null;
+    public static SessionFactory getConnection() {
+
         try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", HOST)
+                    .setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class)
+                    .setProperty("hibernate.c3p0.min_size","5")
+                    .setProperty("hibernate.c3p0.max_size","200")
+                    .setProperty("hibernate.c3p0.max_statements","200");
 
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            e.printStackTrace();
         }
-        return connection;
+        return sessionFactory;
     }
 }
